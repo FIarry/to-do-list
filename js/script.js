5d1c590e8c7d4f98b55f307ddf5e63b7ea959180
flar <122662242+FIarry@users.noreply.github.com>
2024-02-10 14:41:54 +0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -3,71 +3,73 @@ const nameInput = document.querySelector('#nameInput')
 const clrButton = document.querySelector('#clrButton')
 const findInput = document.querySelector('#findInput')
 const list = document.querySelector('#list')
-
+const cancel = document.querySelector('#cancel')
+const accept = document.querySelector('#delete')
+const IND = document.querySelector('#itemNameDelete')
+const frame = document.querySelector('#warning')
 
 let newElementList = function () {
-    const newItem = document.createElement('li');
-    const itemText = document.createElement('span')
-    const strike = document.createElement('s')
-    const cancel = document.querySelector('#cancel')
-    const accept = document.querySelector('#delete')
-    const IND = document.querySelector('#itemNameDelete')
-    const frame = document.querySelector('#warning')
 
-    newItem.classList.add('item')
-    itemText.textContent = nameInput.value
-
-    list.appendChild(newItem)
-    newItem.appendChild(itemText)
-    newItem.appendChild(strike)
-
-    nameInput.value = ''
-
-    let struck = false
-
-    const delBtn = document.createElement('button')
-    delBtn.textContent = 'X'
-    newItem.appendChild(delBtn)
-
-    delBtn.addEventListener('click', () => {
-        frame.classList.remove('hidden')
-        IND.textContent = itemText.textContent
-    })
-
-    accept.addEventListener('click', () => {
-        if (itemText.textContent == IND.textContent) {
-            list.removeChild(newItem)
-        }
-        frame.classList.add('hidden')
-    })
-
-    cancel.addEventListener('click', () => {
-        frame.classList.add('hidden')
-    })
-
-    newItem.addEventListener('click', () => {
-        if (struck == false) {
-            struck = true
-            strike.appendChild(itemText)
-        } else {
-            struck = false
-            newItem.appendChild(itemText)
-        }
-    })
-
-    findInput.addEventListener('keyup', (event) => {
-        if (list.childElementCount > 0) {
-            if (findInput.value != '') {
-                if ((newItem.textContent.toLowerCase()).indexOf((findInput.value).toLowerCase()) == -1) {
-                    newItem.classList.add('hidden')
+    if (nameInput.value != '' && nameInput.value != ' ') {
+        const newItem = document.createElement('li');
+        const itemText = document.createElement('span')
+        const strike = document.createElement('s')
+    
+        newItem.classList.add('item')
+        itemText.textContent = nameInput.value
+    
+        list.appendChild(newItem)
+        newItem.appendChild(itemText)
+        newItem.appendChild(strike)
+    
+        nameInput.value = ''
+    
+        let struck = false
+    
+        const delBtn = document.createElement('button')
+        delBtn.textContent = 'X'
+        newItem.appendChild(delBtn)
+    
+        delBtn.addEventListener('click', () => {
+            frame.classList.remove('hidden')
+            IND.textContent = itemText.textContent
+        })
+    
+        accept.addEventListener('click', () => {
+            if (itemText.textContent == IND.textContent) {
+                list.removeChild(newItem)
+            }
+            frame.classList.add('hidden')
+        })
+    
+        cancel.addEventListener('click', () => {
+            frame.classList.add('hidden')
+        })
+    
+        newItem.addEventListener('click', () => {
+            if (struck == false) {
+                struck = true
+                strike.appendChild(itemText)
+            } else {
+                struck = false
+                newItem.appendChild(itemText)
+            }
+        })
+    
+        findInput.addEventListener('keyup', (event) => {
+            if (list.childElementCount > 0) {
+                if (findInput.value != '') {
+                    if ((newItem.textContent.toLowerCase()).indexOf((findInput.value).toLowerCase()) == -1) {
+                        newItem.classList.add('hidden')
+                    } else {
+                        newItem.classList.remove('hidden')
+                    }
                 } else {
                     newItem.classList.remove('hidden')
                 }
-            } else {
-                newItem.classList.remove('hidden')
             }
-        }
-    })
+        })
+    }
 }
 
 myButton.addEventListener('click', () => {
